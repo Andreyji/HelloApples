fae628da71c7ac444bb518140675608a5c425870
Andreyji <liquidyzer@gmail.com>
2023-03-13 09:53:16 +0200

    changes to vars, main submit

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "aks" {
+  name     = ${var.resource_group}
+  location = ${var.location}
+}
+
+# Create Vnet and subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = var.subnet
+  resource_group_name  = azurerm_resource_group.aks.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}  
+  
+# Create the AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "my-aks-dns"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_DS2_v2"
+    vnet_subnet_id  = "/subscriptions/"${var.subscription_id}"/resourceGroups/"${var.resource_group}/providers/Microsoft.Network/virtualNetworks/"${var.vnet}/subnets/"${var.subnet}"
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+}
--- vars.tf
+++ vars.tf
@@ -25,11 +25,30 @@ variable "tenant_id" {
 variable "location" {
   type        = string
   description = "The location for Azure resources."
+  default 	  = "East US"
 }
 
 variable "resource_group" {
   type		  = string
   description = "The name of the resource group you would like to use."
   default	  = "[ENTER_VALUE]"
-In this example, the vars.tf file defines five variables:
+}
+
+variable "aks_name" {
+  type		  = string
+  description = "The name of the AKS you would like to use."
+  default	  = "my_aks_cluster"
+}
+
+variable "vnet" {
+  type		  = string
+  description = "The name of the v-net you would like to use."
+  default	  = "my-vnet"
+}
+
+variable "subnet" {
+  type		  = string
+  description = "The name of the subnet you would like to use."
+  default	  = "aks_subnet"
+}
 
